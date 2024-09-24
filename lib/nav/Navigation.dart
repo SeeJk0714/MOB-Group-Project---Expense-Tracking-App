7bd640c1b8feb27eeb1b708559ff14f4fad95560
Harden <denishrao22s@forwardschool.edu.my>
2024-09-24 10:54:19 +0800

    update login and register
    mwewrtwe
    
    zxcvbnm,

--- lib/nav/Navigation.dart
+++ lib/nav/Navigation.dart
@@ -1,37 +1,39 @@
 import 'package:expense_tracking_app/screens/bottom_bar_screen.dart';
+import 'package:expense_tracking_app/screens/login_screen.dart';
+import 'package:expense_tracking_app/screens/register_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class Navigation {
-  static const initial = "/";
+  static const initial = "/login";
   static final routes = [
     GoRoute(
       path: "/",
       builder: (context, state) => const BottomBarScreen(),
     ),
-    // GoRoute(
-    //     path: "/home",
-    //     name: Screen.home.name,
-    //     builder: (context, state) => const HomeScreen()),
-    // GoRoute(
-    //     path: "/add",
-    //     name: Screen.add.name,
-    //     builder: (context, state) => const AddScreen()),
-    // GoRoute(
-    //     path: "/search",
-    //     name: Screen.search.name,
-    //     builder: (context, state) => const SearchScreen()),
-    // GoRoute(
-    //     path: "/overview",
-    //     name: Screen.overview.name,
-    //     builder: (context, state) => const OverviewScreen()),
-    // GoRoute(
-    //     path: "/login",
-    //     name: Screen.login.name,
-    //     builder: (context, state) => const LoginScreen()),
-    // GoRoute(
-    //     path: "/register",
-    //     name: Screen.register.name,
-    //     builder: (context, state) => const RegisterScreen()),
+    GoRoute(
+        path: "/home",
+        name: Screen.home.name,
+        builder: (context, state) => const BottomBarScreen()),
+    GoRoute(
+        path: "/add",
+        name: Screen.add.name,
+        builder: (context, state) => const BottomBarScreen()),
+    GoRoute(
+        path: "/search",
+        name: Screen.search.name,
+        builder: (context, state) => const BottomBarScreen()),
+    GoRoute(
+        path: "/overview",
+        name: Screen.overview.name,
+        builder: (context, state) => const BottomBarScreen()),
+    GoRoute(
+        path: "/login",
+        name: Screen.login.name,
+        builder: (context, state) => const LoginScreen()),
+    GoRoute(
+        path: "/register",
+        name: Screen.register.name,
+        builder: (context, state) => const RegisterScreen()),
   ];
 }
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:expense_tracking_app/data/auth/auth_service.dart';
+import 'package:expense_tracking_app/nav/Navigation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +12,175 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _pswController = TextEditingController();
+  final Authservice _authService = Authservice();
+
+  String? _emailError;
+  String? _pswError;
+  bool _isLoading = false;
+
+  Future<void> _login() async {
+    // Clear errors
+    setState(() {
+      _emailError = null;
+      _pswError = null;
+      _isLoading = true;
+    });
+
+    // Basic validation
+    if (_emailController.text.isEmpty) {
+      setState(() {
+        _emailError = "Please enter your email";
+        _isLoading = false;
+      });
+      return;
+    }
+    if (_pswController.text.isEmpty) {
+      setState(() {
+        _pswError = "Please enter your password";
+        _isLoading = false;
+      });
+      return;
+    }
+
+    try {
+      User? user = await _authService.signInWithEmailPassword(
+          _emailController.text, _pswController.text);
+      if (user != null) {
+        await context.pushNamed(Screen.home.name);
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        if (e.code == 'user-not-found') {
+          _emailError = "Email does not exist";
+        } else if (e.code == 'wrong-password') {
+          _pswError = "Incorrect Password";
+        } else {
+          _emailError = "Login failed. Please try again";
+        }
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.login_outlined,
+                    size: 50.0,
+                    color: Colors.white,
+                  ),
+                  Icon(
+                    Icons.person_add_alt_1_outlined,
+                    size: 50.0,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40.0),
+              Text(
+                "Welcome\nBack",
+                style: TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.bold,
+                    foreground: Paint()..color = Colors.white),
+              ),
+              const SizedBox(height: 120.0),
+              TextField(
+                controller: _emailController,
+                onChanged: (value) => setState(() {
+                  _emailError = null; // Clear error on typing
+                }),
+                style: const TextStyle(fontSize: 22.0, color: Colors.black),
+                decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle:
+                        const TextStyle(fontSize: 22.0, color: Colors.black54),
+                    errorText: _emailError, // Show error message here
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xFFFFE7E7)),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: _pswController,
+                onChanged: (value) => setState(() {
+                  _pswError = null; // Clear error on typing
+                }),
+                style: const TextStyle(fontSize: 22.0),
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    labelStyle:
+                        const TextStyle(fontSize: 22.0, color: Colors.black54),
+                    errorText: _pswError, // Show error message here
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xFFFFE7E7)),
+              ),
+              const SizedBox(height: 140),
+              _isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : FilledButton(
+                      style: FilledButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        backgroundColor: const Color(0xFFB47B84),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      onPressed: _login,
+                      child: const Text('Login',
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            color: Colors.white,
+                          )),
+                    ),
+              const SizedBox(height: 10.0),
+              Center(
+                child: TextButton(
+                    onPressed: () {
+                      context.pushNamed(Screen.register.name);
+                    },
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Color(0xFFB47B84),
+                        decoration: TextDecoration.underline,
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
