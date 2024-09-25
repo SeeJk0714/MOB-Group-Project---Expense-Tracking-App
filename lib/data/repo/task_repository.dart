7dd1eb0391cb4f33465d882e6d40ad29b77425c0
jiekai <jiekai22s@forwardschool.edu.my>
2024-09-25 18:03:03 +0800

    update add collection

--- lib/data/repo/task_repository.dart
+++ lib/data/repo/task_repository.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_tracking_app/data/model/task.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TaskRepository {
   static TaskRepository instance = TaskRepository._init();
@@ -10,20 +11,31 @@ class TaskRepository {
 
   TaskRepository._init();
 
-  final _collection = FirebaseFirestore.instance.collection("tasks");
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  CollectionReference getCollection() {
+    final User? user = _auth.currentUser;
+    if (user == null) {
+      throw Exception("User ID doesn't exist");
+    }
+    return _firestore.collection('root_db/${user.uid}/tasks');
+  }
 
   Stream<List<Task>> getTasksStream() {
-    return _collection.snapshots().map((event) => event.docs
-        .map((doc) => Task.fromMap(doc.data()).copy(id: doc.id))
+    return getCollection().snapshots().map((event) => event.docs
+        .map((doc) =>
+            Task.fromMap(doc.data() as Map<String, dynamic>).copy(id: doc.id))
         .toList());
   }
 
   Future<List<Task>> getAllTasks() async {
-    final res = await _collection.get();
+    final res = await getCollection().get();
     final List<Task> tasks = [];
 
     for (var doc in res.docs) {
-      final Task task = Task.fromMap(doc.data()).copy(id: doc.id);
+      final Task task =
+          Task.fromMap(doc.data() as Map<String, dynamic>).copy(id: doc.id);
       tasks.add(task);
     }
 
@@ -31,24 +43,24 @@ class TaskRepository {
   }
 
   Future<Task?> getTaskById(String id) async {
-    final res = await _collection.doc(id).get();
+    final res = await getCollection().doc(id).get();
     if (res.data() == null) {
       return null;
     }
 
-    return Task.fromMap(res.data()!).copy(id: res.id);
+    return Task.fromMap(res.data() as Map<String, dynamic>).copy(id: res.id);
   }
 
   Future<void> addTask(Task task) async {
-    await _collection.add(task.toMap());
+    await getCollection().add(task.toMap());
   }
 
   Future<void> updateTask(Task task) async {
-    await _collection.doc(task.id!).set(task.toMap());
+    await getCollection().doc(task.id!).set(task.toMap());
   }
 
   Future<void> deleteTask(String id) async {
-    await _collection.doc(id).delete();
+    await getCollection().doc(id).delete();
   }
 
   Future<String> greet() {
